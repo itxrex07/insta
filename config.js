b8e2d28b0ec1d70b47df6c921f3cb446430fc648
Muhammad Arshman <itxarshman@gmail.com>
2025-07-24 14:35:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,6 @@ export const config = {
   instagram: {
     username: 'itxrey', // Your Instagram username
     password: 'your_instagram_password', // Your Instagram password
-    sessionPath: './session/session.json',
-    messageCheckInterval: 5000, // Check for messages every 5 seconds
-    maxRetries: 3,
     useMongoSession: true // Set to false to use file-based sessions
   },
   
@@ -19,10 +16,6 @@ export const config = {
   mongo: {
     uri: 'mongodb+srv://itxelijah07:ivp8FYGsbVfjQOkj@cluster0.wh25x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     dbName: 'hyper_insta',
-    options: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
   },
   
   modules: {
